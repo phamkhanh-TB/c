60d4c861d891df8631d437214bee672f70c59318
Khánh Phạm <davidkhanh206@gmail.com>
2024-07-27 09:55:30 +0700

    gần hiểu rồi

---
+++ sliding window.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;cin>>num;
+    while(num--){
+        int n,k;cin>>n>>k;
+        int a[1000];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        long long sum=0;
+        for (int i=0;i<k;i++){
+            sum+=a[i];
+        }
+        for (int i=k;i<n;i++){
+            
+        }
+    }
+}
